4aa716380f5afca9f092a9afa9196ee5adc39aa1
Samuel Sumeet Raj Kumar <samuel.rajkumar@utdallas.edu>
2023-07-10 00:10:26 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>User Interface Example</h1>
+      <p>Welcome to my TypeScript React app!</p>
+    </div>
+  );
+};
+
+export default App;
